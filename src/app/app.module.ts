f54a90909aba93fa91138f3a011e266005840fad
Jorge Alvarez <jorgehaq@gmail.com>
2024-01-22 21:28:46 -0500

    filters in component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
+import { FiltersComponent } from './pages/home/components/filters/filters.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { ProductsHeaderComponent } from './pages/home/components/products-header
     HeaderComponent,
     HomeComponent,
     ProductsHeaderComponent,
+    FiltersComponent,
   ],
   imports: [
     BrowserModule,
