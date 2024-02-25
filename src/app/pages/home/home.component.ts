05d2ff2480bc016dd34931698bcafd5d461261af
Jorge Alvarez <jorgehaq@gmail.com>
2024-02-24 23:21:09 -0500

    Adding cart iitems

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,31 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.models';
+import { CartService } from 'src/app/services/cart.service';
 
-const ROWS_HEIGTH: {[id:number]:number} ={ 1:400, 3: 335, 4:350};
+const ROWS_HEIGTH: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 
 @Component({
   selector: 'app-home',
-  templateUrl:'./home.component.html',
+  templateUrl: './home.component.html',
   styles: [
   ]
 })
 export class HomeComponent implements OnInit {
-
-  cols=3;
-  rowsHeight=ROWS_HEIGTH[this.cols];
+  
+  cols = 3;
+  rowsHeight = ROWS_HEIGTH[this.cols];
   category: string | undefined;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
   }
 
-  onColumnsCountChange(colsNum:number):void{
-    this.cols=colsNum;
-    this.rowsHeight=ROWS_HEIGTH[this.cols];
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+    this.rowsHeight = ROWS_HEIGTH[this.cols];
   }
 
-  onShowCategory(newCategory:string):void{
-    this.category=newCategory;
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
+
+  onAddToCart(product: Product): void {
+    this.cartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id
+    })
   }
 
 }
